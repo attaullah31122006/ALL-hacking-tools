8ae2b580dd16bcf14971c2ad6cf9e2210f37175b
Attaullah <143985528+attaullah31122006@users.noreply.github.com>
2023-10-10 12:40:38 +0500

    Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -1,18 +1,31 @@
 #!/usr/bin
 #Copyright 2021 AllHackingTools
-#Written by : Misha Korzhik
-#Github     : http://github.com/mishakorzik
+#Written by : A.K The Hacker
+#Github     : http://github.com/attaullah31122006
 
 echo '
 [1]
- |  ██╗░░██╗░█████╗░░█████╗░██╗░░██╗██╗███╗░░██╗░██████╗░░░░░░░████████╗░█████╗░░█████╗░██╗░░░░░
- |  ██║░░██║██╔══██╗██╔══██╗██║░██╔╝██║████╗░██║██╔════╝░░░░░░░╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░
- |  ███████║███████║██║░░╚═╝█████═╝░██║██╔██╗██║██║░░██╗░█████╗░░░██║░░░██║░░██║██║░░██║██║░░░░░
- |  ██╔══██║██╔══██║██║░░██╗██╔═██╗░██║██║╚████║██║░░╚██╗╚════╝░░░██║░░░██║░░██║██║░░██║██║░░░░░
- |  ██║░░██║██║░░██║╚█████╔╝██║░╚██╗██║██║░╚███║╚██████╔╝░░░░░░░░░██║░░░╚█████╔╝╚█████╔╝███████╗
- |  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ v2.7.4
+ 
+                                                             
+                                                             
+               AAA                       KKKKKKKKK    KKKKKKK
+              A:::A                      K:::::::K    K:::::K
+             A:::::A                     K:::::::K    K:::::K
+            A:::::::A                    K:::::::K   K::::::K
+           A:::::::::A                   KK::::::K  K:::::KKK
+          A:::::A:::::A                    K:::::K K:::::K   
+         A:::::A A:::::A                   K::::::K:::::K    
+        A:::::A   A:::::A                  K:::::::::::K     
+       A:::::A     A:::::A                 K:::::::::::K     
+      A:::::AAAAAAAAA:::::A                K::::::K:::::K    
+     A:::::::::::::::::::::A               K:::::K K:::::K   
+    A:::::AAAAAAAAAAAAA:::::A            KK::::::K  K:::::KKK
+   A:::::A             A:::::A           K:::::::K   K::::::K
+  A:::::A               A:::::A   ...... K:::::::K    K:::::K
+ A:::::A                 A:::::A  .::::. K:::::::K    K:::::K
+AAAAAAA                   AAAAAAA ...... KKKKKKKKK    KKKKKKK     v2.7.4
 [3] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
-[3] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[2] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[3] Github   : https://github.com/attaullah31122006  -  nsi⁣ble responsible for any consequences of reading it. 
+[2] Coded By : A.K The Hacker (Міша Коржик)  -  All materials are provided for educational purposes only! 
 [4]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[715]
 '|lolcat -p 1.6 -a -d 2 -s 20.0
